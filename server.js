9f6cda229ef6c12f678bb7e6e603cc726fc81baa
PlamenPetrovMMS <flamepetrov01@gmail.com>
2025-10-27 14:07:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ const pool = new Pool({
 });
 
 app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-  await pool.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
-  res.send("User registered!");
+    console.log("🔐 Registering a new user");
+    const { email, password } = req.body;
+    await pool.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
+    res.send("User registered!");
 });
 
 app.listen(3000, () => console.log("✅ Server running on port 3000"));
